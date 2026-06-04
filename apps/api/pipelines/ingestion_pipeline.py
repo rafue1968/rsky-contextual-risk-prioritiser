9251f43d2822ed61403a9ccc149cd4395d097981
rafue1968 <rafue1968@gmail.com>
2026-06-04 16:40:59 +0100

    refractor: updates

--- apps/api/pipelines/ingestion_pipeline.py
+++ apps/api/pipelines/ingestion_pipeline.py
@@ -1,17 +1,32 @@
 from connectors.zap import parse_zap_file
 from normalisers.core import normalise_finding
+import logging
+
+logger = logging.getLogger(__name__)
 
 def run_ingestion_pipeline(file_path: str):
+
     """
     End-to-end ingestion pipeline:
     Connector → Normaliser
     """
 
-    raw_findings = parse_zap_file("zap.json")
+    logger.info("Starting ingestion pipeline")
+
+    # raw_findings = parse_zap_file("zap.json")
+    raw_findings = parse_zap_file(file_path)
+
+    if not raw_findings:
+        logger.error("No findings found")
+        return []
+
+    logger.info(f"Found {len(raw_findings)} findings")
 
     final_findings = [
         normalise_finding(f)
         for f in raw_findings
     ]
+
+    logger.info(f"Found {len(final_findings)} normalised findings")
 
     return final_findings
